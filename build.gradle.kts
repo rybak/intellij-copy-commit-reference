04f664c464f87ec296b3e06b0a4ad0dd731049d6
Andrei Rybak <rybak.a.v@gmail.com>
2023-06-25 13:22:35 +0200

    plugin.xml: remove until-build attribute

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,9 @@ tasks {
 	patchPluginXml {
 		version = properties("pluginVersion")
 		sinceBuild = properties("pluginSinceBuild")
-		untilBuild = properties("pluginUntilBuild")
+		// attribute `until-build` is optional
+		// https://plugins.jetbrains.com/docs/intellij/plugin-configuration-file.html#idea-plugin__idea-version
+		// untilBuild = properties("pluginUntilBuild")
 
 		// Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
 		pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
